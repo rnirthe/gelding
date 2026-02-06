30bc22823f7c8bca8bac6aee1f80ae7ef8019d3e
Mirthe Winter <mirthe.winter@gnorrel.nl>
2026-02-06 22:19:51 +0100

    skeleton crew

---
+++ ui.py
@@ -0,0 +1,31 @@
+# left tools area
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# right months area
+#
+# ----- old month ----- (faded)
+#       1 line summary  (faded)
+# ----- old month ----- (faded)
+#       1 line summary  (faded)
+# ----- old month ----- (faded)
+#       1 line summary  (faded)
+# ----- old month ----- (faded)
+#       1 line summary  (faded)
+#
+# ----- current month -----
+#       summary including current state of accounts
+#       table
+#
+# ----- future month -----
+#       1 line summary
+# ----- future month -----
+#       1 line summary
+# ----- future month -----
+#       1 line summary
